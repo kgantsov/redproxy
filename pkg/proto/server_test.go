7b2c6123644f75054674f63a02abee9e40954773
Kostiantyn Hantsov <k.gantsov@gmail.com>
2022-09-30 21:36:50 +0200

    Server tests improvements

--- pkg/proto/server_test.go
+++ pkg/proto/server_test.go
@@ -11,15 +11,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func setupClients() map[string]RedisClient {
+	clients := map[string]RedisClient{}
+
+	redisClient1 := NewMockRedisClient(
+		map[string]string{"k6": "value_6", "k213": "value_213", "k32151": "value_32151"},
+	)
+	clients["localhost:16379"] = redisClient1
+
+	redisClient2 := NewMockRedisClient(
+		map[string]string{"k0": "value_0", "k1": "value_1", "k5": "value_5", "k9": "value_9", "k10": "value_10"},
+	)
+	clients["localhost:26379"] = redisClient2
+
+	redisClient3 := NewMockRedisClient(
+		map[string]string{"k2": "value_2", "k3": "value_3", "k4": "value_4", "k7": "value_7", "k8": "value_8"},
+	)
+	clients["localhost:36379"] = redisClient3
+
+	return clients
+}
+
 func TestServerGet(t *testing.T) {
 	port := 46379
 
-	redisClient := NewMockRedisClient(
-		map[string]string{"k1": "v1", "k2": "2", "k3": "value", "year": "2022"},
-	)
-
-	redises := map[string]RedisClient{}
-	redises["localhost:6379"] = redisClient
+	redises := setupClients()
 
 	_proxy := NewRedisProxy(redises)
 	server := NewServer(_proxy, port)
@@ -37,10 +53,17 @@ func TestServerGet(t *testing.T) {
 		key  string
 		want string
 	}{
-		{key: "k1", want: "v1", err: nil},
-		{key: "k2", want: "2", err: nil},
-		{key: "k3", want: "value", err: nil},
-		{key: "year", want: "2022", err: nil},
+		{key: "k0", want: "value_0", err: nil},
+		{key: "k1", want: "value_1", err: nil},
+		{key: "k2", want: "value_2", err: nil},
+		{key: "k3", want: "value_3", err: nil},
+		{key: "k4", want: "value_4", err: nil},
+		{key: "k5", want: "value_5", err: nil},
+		{key: "k6", want: "value_6", err: nil},
+		{key: "k7", want: "value_7", err: nil},
+		{key: "k8", want: "value_8", err: nil},
+		{key: "k213", want: "value_213", err: nil},
+		{key: "k32151", want: "value_32151", err: nil},
 		{key: "foo", want: "", err: redis.Nil},
 	}
 
@@ -58,12 +81,7 @@ func TestServerGet(t *testing.T) {
 func TestServerSet(t *testing.T) {
 	port := 56379
 
-	redisClient := NewMockRedisClient(
-		map[string]string{"k1": "v1", "k2": "2", "k3": "value", "year": "2022"},
-	)
-
-	redises := map[string]RedisClient{}
-	redises["localhost:6379"] = redisClient
+	redises := setupClients()
 
 	_proxy := NewRedisProxy(redises)
 	server := NewServer(_proxy, port)
@@ -98,12 +116,7 @@ func TestServerSet(t *testing.T) {
 func TestServerDel(t *testing.T) {
 	port := 36379
 
-	redisClient := NewMockRedisClient(
-		map[string]string{"k1": "v1", "k2": "2", "k3": "value", "year": "2022"},
-	)
-
-	redises := map[string]RedisClient{}
-	redises["localhost:6379"] = redisClient
+	redises := setupClients()
 
 	_proxy := NewRedisProxy(redises)
 	server := NewServer(_proxy, port)
@@ -120,15 +133,15 @@ func TestServerDel(t *testing.T) {
 
 	val, err := client.Get(ctx, "k1").Result()
 	assert.Equal(t, nil, err, "they should be equal")
-	assert.Equal(t, "v1", val, "they should be equal")
+	assert.Equal(t, "value_1", val, "they should be equal")
 
 	val, err = client.Get(ctx, "k2").Result()
 	assert.Equal(t, nil, err, "they should be equal")
-	assert.Equal(t, "2", val, "they should be equal")
+	assert.Equal(t, "value_2", val, "they should be equal")
 
 	val, err = client.Get(ctx, "k3").Result()
 	assert.Equal(t, nil, err, "they should be equal")
-	assert.Equal(t, "value", val, "they should be equal")
+	assert.Equal(t, "value_3", val, "they should be equal")
 
 	deleted := client.Del(ctx, "k1", "k3").Val()
 
@@ -140,7 +153,7 @@ func TestServerDel(t *testing.T) {
 
 	val, err = client.Get(ctx, "k2").Result()
 	assert.Equal(t, nil, err, "they should be equal")
-	assert.Equal(t, "2", val, "they should be equal")
+	assert.Equal(t, "value_2", val, "they should be equal")
 
 	val, err = client.Get(ctx, "k3").Result()
 	assert.Equal(t, redis.Nil, err, "they should be equal")
@@ -151,12 +164,7 @@ func TestServerDel(t *testing.T) {
 func TestServerKeys(t *testing.T) {
 	port := 46379
 
-	redisClient := NewMockRedisClient(
-		map[string]string{"k1": "v1", "k2": "2", "k3": "value", "year": "2022"},
-	)
-
-	redises := map[string]RedisClient{}
-	redises["localhost:6379"] = redisClient
+	redises := setupClients()
 
 	_proxy := NewRedisProxy(redises)
 	server := NewServer(_proxy, port)
@@ -174,10 +182,11 @@ func TestServerKeys(t *testing.T) {
 		key  string
 		want []string
 	}{
-		{key: "k*", want: []string{"k1", "k2", "k3"}, err: nil},
+		{key: "k*", want: []string{"k0", "k1", "k10", "k2", "k213", "k3", "k32151", "k4", "k5", "k6", "k7", "k8", "k9"}, err: nil},
 		{key: "k2", want: []string{"k2"}, err: nil},
-		{key: "*", want: []string{"k1", "k2", "k3", "year"}, err: nil},
-		{key: "year", want: []string{"year"}, err: nil},
+		{key: "*", want: []string{"k0", "k1", "k10", "k2", "k213", "k3", "k32151", "k4", "k5", "k6", "k7", "k8", "k9"}, err: nil},
+		{key: "k3215*", want: []string{"k32151"}, err: nil},
+		{key: "k32151", want: []string{"k32151"}, err: nil},
 		{key: "foo", want: []string{}, err: nil},
 	}
 
